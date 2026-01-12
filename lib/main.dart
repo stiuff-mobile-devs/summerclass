aa15edb7264dd2a8d8a62cb3ba571e2a868b9a9e
JpFontinele-UFF <jfontinele@id.uff.br>
2026-01-12 10:00:20 -0300

    feat-aula4 configurando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:summerclass/app/routes/app_pages.dart';
 import 'package:summerclass/app/routes/app_routes.dart';
+import 'package:summerclass/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     GetMaterialApp(initialRoute: Routes.SPLASH, getPages: AppPages.routes),
   );
